2a71df18168b9d27a60a204daaa0a385bdc7c6b0
Jonny <53932961+Project-Jonny@users.noreply.github.com>
2021-11-07 00:59:03 +0900

    Bananaゲージの実装

--- Assets/Resources/Scripts/BananaMove.cs
+++ Assets/Resources/Scripts/BananaMove.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // バナナにつけるScript
 public class BananaMove : MonoBehaviour
 {
     float speed;
 
+    [SerializeField] GameObject bananaUI;
+    Slider bananaSlider;
+
     void Start()
     {
         speed = ParamsSO.Entity.bananaSpeed;
+        bananaSlider = bananaUI.transform.Find("Banana Bar").GetComponent<Slider>();
+        bananaSlider.value = 0f;
     }
 
     void Update()
@@ -22,5 +28,14 @@ public class BananaMove : MonoBehaviour
             // バナナ（自分自身）を消してね
             Destroy(gameObject);
         }
+    }
+
+    public void UpdateValue()
+    {
+        if (bananaUI.activeSelf == false)
+        {
+            bananaUI.SetActive(true);
+        }
+        bananaSlider.value += 0.05f;
     }
 }
--- Assets/Resources/Scripts/Bullet.cs
+++ Assets/Resources/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed;
+    bool hit;
 
     void Start()
     {
@@ -13,13 +14,27 @@ public class Bullet : MonoBehaviour
 
     void Update()
     {
-        transform.position += new Vector3(0, speed, 0);
-
-        // もし弾が6より上に移動したら
-        if (transform.position.y >= 6)
+        if (!hit)
         {
-            // 弾（自分自身）を消してね
-            Destroy(gameObject);
+            transform.position += new Vector3(0, speed, 0);
+
+            // もし弾が6より上に移動したら
+            if (transform.position.y >= 6)
+            {
+                // 弾（自分自身）を消してね
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Banana")
+        {
+            hit = true;
+            gameObject.transform.parent = collision.gameObject.transform;
+
+            collision.gameObject.GetComponent<BananaMove>().UpdateValue();
         }
     }
 }
